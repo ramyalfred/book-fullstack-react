d80658d470adc742a07385711932a72115d846d0
ramyalfred <ramy.alfred92@gmail.com>
2021-08-08 01:17:52 +0200

    Steps till page 64

--- exercise-files/voting_app/public/js/app-2.js
+++ exercise-files/voting_app/public/js/app-2.js
@@ -1,40 +1,99 @@
 
 class ProductList extends React.Component {
+
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      productList: window.Seed.products,
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    
+  }
+
   render() {
+
     return (
       <div className='ui unstackable items'>
-        <Product />
+
+        {this.state.productList.sort((a,b) => (b.votes - a.votes)).map((item) => (
+          <Product key={'product-' + item.id} {...item} onVote={this.handleProductUpVote}/>
+        ))}
+        
       </div>
     );
+  }
+
+  handleProductUpVote(id){
+    console.log(id);
+
+    window.Seed.products.forEach((product) => {
+      if (product.id === id) {
+        product.votes = product.votes + 1;
+      }
+    });
+
+    this.setState({
+      productList: window.Seed.products
+    });
   }
 }
 
 class Product extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
   render() {
+    
+    const{id,
+      title,
+      description,
+      url,
+      votes,
+      submitterAvatarUrl,
+      productImageUrl,
+      onVote} = this.props;
+
     return (
-      <div className='item'>
-        <div className='image'>
-          <img src='images/products/image-aqua.png' />
+      <div className="item">
+        <div className="image">
+          <img src={productImageUrl}/>
         </div>
         <div className='middle aligned content'>
+          <div className='header'>
+          <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {votes}
+          </div>
           <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={url}>{title}</a>
+            <p>{description}</p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='ui avatar image'
-              src='images/avatars/daniel.jpg'
+              src={submitterAvatarUrl}
             />
           </div>
         </div>
       </div>
     );
   }
+
+  handleUpVote(){
+    this.props.onVote(this.props.id);
+  }
 }
 
 ReactDOM.render(
-  <ProductList />,
+  <ProductList/>, 
   document.getElementById('content')
 );
