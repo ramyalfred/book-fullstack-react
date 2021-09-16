a99720f74db186924a1d43fe51566c4fdd605a53
ramyalfred <ramy.alfred92@gmail.com>
2021-09-16 22:40:49 +0200

    Reading timers from data json file

--- exercise-files/time_tracking_app/public/js/app.js
+++ exercise-files/time_tracking_app/public/js/app.js
@@ -10,22 +10,7 @@ class TimerDashboard extends React.Component {
   }
 
     state = {
-        timers: [
-          {
-            title: 'Practice squat',
-            project: 'Gym Chores',
-            id: uuid.v4(),
-            elapsed: 5456099,
-            runningSince: Date.now(),
-          },
-          {
-            title: 'Bake squash',
-            project: 'Kitchen Chores',
-            id: uuid.v4(),
-            elapsed: 1273998,
-            runningSince: Date.now(),
-          },
-        ],
+        timers: [],
     };
 
    handleEditFormSubmit = (attrs) => {
@@ -127,6 +112,17 @@ class TimerDashboard extends React.Component {
         </div>
       </div>
       );
+    }
+
+    componentDidMount(){
+      this.loadTimersFromServer();
+      setInterval(this.loadTimersFromServer,5000);
+    }
+
+    loadTimersFromServer = () => {
+      client.getTimers((serverTimers) => {
+        this.setState({timers: serverTimers});
+      });
     }
 }
 
